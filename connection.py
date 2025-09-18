091c0fca1a6f6eeff2fbba1e0c04e4face395f61
Renan <renan_dejesus24@hotmail.com>
2025-09-17 21:37:52 -0300

    feat: script to connect with DB

--- connection.py
+++ connection.py
@@ -1,18 +1,25 @@
-import mysql.connector
+import pymysql
 
-def connection(db_path="Banco/DB.sql"):
+def connect():
+    print("Tentando conectar ao MySQL...")
+
     try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="admin",
-            database="Documents"
+        conn = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='admin',
+            database='documents'
         )
-        return conn
-    except mysql.connector.Error as err:
-        print(f"Erro ao conectar ao banco de dados: {err}")
-        return None
+        if conn.open:
+            return conn
 
-def close_connection(conn):
-    if conn.is_connected():
-        conn.close()
+    except pymysql.MySQLError as e:
+        print(f"Erro de conexão: {e}")
+        
+def closeConnection(conn):
+    try:
+        if conn:
+            conn.close()
+            print("Conexão fechada.")
+    except pymysql.MySQLError as e:
+        print(f"Erro ao fechar a conexão: {e}")
